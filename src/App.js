0dc85b1fcc50d5163788ec6439b92ac38c0003df
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-06-08 08:08:09 -0300

    adicionar componente Filter ao APP

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import MyProvider from './context/MyProvider';
 import Table from './components/Table';
 import Header from './components/Header';
+import Filter from './components/Filter';
 import './App.css';
 
 function App() {
   return (
     <MyProvider>
       <Header />
+      <Filter />
       <Table />
     </MyProvider>
   );
