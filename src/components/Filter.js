707380e25b38b800b6a014f326c82178b5658840
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-06-08 08:05:56 -0300

    criar um componente Filter

---
+++ src/components/Filter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Filter() {
+  return (
+    <button
+      type="button"
+      data-testid="button-filter"
+    >
+      Filtrar
+    </button>
+  );
+}
+
+export default Filter;
