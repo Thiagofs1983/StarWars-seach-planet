c981af2ade5f2228b33f9c28c7eacdb70d5f98f3
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-06-13 15:15:57 -0300

    adiciona estilo ao componente Filter

--- src/Style.js
+++ src/Style.js
@@ -34,10 +34,71 @@ export const ButtonTrash = styled.button`
   border: none;
   color: red;
   font-size: 16px;
+  transition: 0.2s;
   
   :hover {
     color: rgb(200, 1, 1);
     cursor: pointer;
     font-size: 17px;
   }
+`;
+
+export const FilterS = styled.form`
+  display: flex;
+  height: 60px;
+  align-items: center;
+`;
+
+export const FilterNumeric = styled.section`
+  display: flex;
+  justify-content: space-around;
+  width: 50%;
+`;
+
+export const Label = styled.label`
+  width: 150px;
+  align-self: center;
+`;
+
+export const Select = styled.select`
+  background-color: transparent;
+  color: rgb(148, 161, 178);
+  border-color: transparent transparent rgb(148, 161, 178);
+  outline: none;
+  text-align: start;
+  width: 115px;
+  :hover {
+    background-color: rgb(26, 26, 26);
+    cursor: pointer;
+  }
+`;
+
+export const InputFilter = styled.input`
+  background-color: transparent;
+  color: rgb(148, 161, 178);
+  border: 1px solid rgb(148, 161, 178);
+  border-radius: 8px;
+  text-align: center;
+  width: 140px;
+  padding: 0;
+`;
+
+export const Button = styled.button`
+  background-color: rgb(127, 90, 240);
+  box-shadow: 0 3px 3px 0 rgb(1, 1, 1);
+  color: rgb(255, 255, 254);
+  transition: 0.2s;
+  :hover {
+    box-shadow: none;
+    border: none;
+    cursor: pointer;
+  }
+  :active {
+    background-color: white;
+  }
+`;
+
+export const Radio = styled.section`
+  display: flex;
+  flex-direction: column;
 `;
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,5 +1,14 @@
 import React, { useContext } from 'react';
 import MyContext from '../context/MyContext';
+import {
+  FilterS,
+  FilterNumeric,
+  Select,
+  InputFilter,
+  Label,
+  Button,
+  Radio,
+} from '../Style';
 
 function Filter() {
   const {
@@ -14,104 +23,110 @@ function Filter() {
     handleClickSort,
   } = useContext(MyContext);
   return (
-    <div>
-      <label htmlFor="column">
-        Coluna
-        <select
-          name="column"
-          id="column"
-          value={ filterByNumericValues.column }
+    <FilterS>
+      <FilterNumeric>
+        <Label htmlFor="column">
+          Coluna
+          <Select
+            name="column"
+            id="column"
+            value={ filterByNumericValues.column }
+            onChange={ handleChangeFilter }
+            data-testid="column-filter"
+          >
+            {
+              optionColunm.map((option) => (
+                <option key={ option }>{ option }</option>
+              ))
+            }
+          </Select>
+        </Label>
+        <Label htmlFor="comparison">
+          Operador
+          <Select
+            name="comparison"
+            id="comparison"
+            value={ filterByNumericValues.comparison }
+            onChange={ handleChangeFilter }
+            data-testid="comparison-filter"
+          >
+            <option value="maior que">maior que</option>
+            <option value="menor que">menor que</option>
+            <option value="igual a">igual a</option>
+          </Select>
+        </Label>
+        <InputFilter
+          type="number"
+          name="value"
+          value={ filterByNumericValues.value }
           onChange={ handleChangeFilter }
-          data-testid="column-filter"
-        >
-          {
-            optionColunm.map((option) => (
-              <option key={ option }>{ option }</option>
-            ))
-          }
-        </select>
-      </label>
-      <label htmlFor="comparison">
-        Operador
-        <select
-          name="comparison"
-          id="comparison"
-          value={ filterByNumericValues.comparison }
-          onChange={ handleChangeFilter }
-          data-testid="comparison-filter"
-        >
-          <option value="maior que">maior que</option>
-          <option value="menor que">menor que</option>
-          <option value="igual a">igual a</option>
-        </select>
-      </label>
-      <input
-        type="number"
-        name="value"
-        value={ filterByNumericValues.value }
-        onChange={ handleChangeFilter }
-        data-testid="value-filter"
-      />
-      <button
-        type="button"
-        onClick={ handleClickFilter }
-        data-testid="button-filter"
-      >
-        Filtrar
-      </button>
-      <button
-        type="button"
-        onClick={ removeAllFilters }
-        data-testid="button-remove-filters"
-      >
-        REMOVER FILTROS
-      </button>
-      <label htmlFor="column">
-        Ordenar
-        <select
-          name="column"
-          id="column"
-          value={ order.column }
-          onChange={ handleChangeSort }
-          data-testid="column-sort"
-        >
-          {
-            optionColunm.map((option) => (
-              <option key={ option }>{option}</option>
-            ))
-          }
-        </select>
-      </label>
-      <label htmlFor="ASC">
-        <input
-          type="radio"
-          name="sort"
-          value="ASC"
-          id="ASC"
-          data-testid="column-sort-input-asc"
-          onClick={ handleClickRadio }
+          data-testid="value-filter"
         />
-        Ascendente
-      </label>
-      <label htmlFor="DESC">
-        <input
-          type="radio"
-          name="sort"
-          value="DESC"
-          id="DESC"
-          data-testid="column-sort-input-desc"
-          onClick={ handleClickRadio }
-        />
-        Descendente
-      </label>
-      <button
-        type="button"
-        onClick={ handleClickSort }
-        data-testid="column-sort-button"
-      >
-        ORDENAR
-      </button>
-    </div>
+        <Button
+          type="button"
+          onClick={ handleClickFilter }
+          data-testid="button-filter"
+        >
+          Filtrar
+        </Button>
+      </FilterNumeric>
+      <FilterNumeric>
+        <Label htmlFor="column">
+          Ordenar
+          <Select
+            name="column"
+            id="column"
+            value={ order.column }
+            onChange={ handleChangeSort }
+            data-testid="column-sort"
+          >
+            {
+              optionColunm.map((option) => (
+                <option key={ option }>{option}</option>
+              ))
+            }
+          </Select>
+        </Label>
+        <Radio>
+          <Label htmlFor="ASC">
+            <input
+              type="radio"
+              name="sort"
+              value="ASC"
+              id="ASC"
+              data-testid="column-sort-input-asc"
+              onClick={ handleClickRadio }
+            />
+            Ascendente
+          </Label>
+          <Label htmlFor="DESC">
+            <input
+              type="radio"
+              name="sort"
+              value="DESC"
+              id="DESC"
+              data-testid="column-sort-input-desc"
+              onClick={ handleClickRadio }
+            />
+            Descendente
+          </Label>
+        </Radio>
+        <Button
+          type="button"
+          onClick={ handleClickSort }
+          data-testid="column-sort-button"
+        >
+          ORDENAR
+        </Button>
+        <Button
+          type="button"
+          onClick={ removeAllFilters }
+          data-testid="button-remove-filters"
+        >
+          REMOVER FILTROS
+        </Button>
+      </FilterNumeric>
+    </FilterS>
   );
 }
 
